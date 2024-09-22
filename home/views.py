43f6e90966f95689066111116383b52f2949d665
Lucas <lucas44794@hotmail.com>
2024-09-22 13:27:48 -0300

    teste

--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Oiii')
+    return render(request, 'home/index.html')
+
+def contato(request):
+    return render(request, 'home/contato.html')
